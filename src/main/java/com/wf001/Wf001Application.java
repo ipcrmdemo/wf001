4afdadd5c2090c9c382d5e7a3223ca028ecf803e
Matt Cadorette <mcadorette@gmail.com>
2019-01-23 16:22:44 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/wf001/Wf001Application.java
@@ -0,0 +1,12 @@
+package com.wf001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wf001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wf001Application.class, args);
+	}
+}
